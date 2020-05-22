ed630492eb74d639291a435473c0aad351a38b1c
Charlie <traceurling@gmail.com>
2020-05-22 09:21:59 -0700

    prototypes

--- challenges/closure.js
+++ challenges/closure.js
@@ -23,11 +23,13 @@ one level above it but still within its scope and access it.
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
-function sumation(x){
-  if (x == 1){
-    return 1;
-  }
-  return x + sumation(x-1);
-}
+// counter = 0;
+// function sumation(x){
+//   counter ++
+//   sumation(x-1);
+//   if (x == 1){
+//     return counter;
+//   }
+// }
 
-console.log(sumation(4));
+// console.log(sumation(4));
